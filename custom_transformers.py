893431928fbf685a9abaefb19448126504469b90
johnprg <john@theartofwork.com>
2024-12-18 18:09:55 -0800

    remove debugging print statements and allow pandas series

--- custom_transformers.py
+++ custom_transformers.py
@@ -2,6 +2,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+import pandas as pd
 
 class LemmatizerStemmer(BaseEstimator, TransformerMixin):
     def __init__(self):
@@ -12,14 +13,14 @@ class LemmatizerStemmer(BaseEstimator, TransformerMixin):
         return self
     
     def transform(self, X):
-        # Ensure X is a list of strings
-        print(f"Input to transform: {X}")  # Debugging statement
-        if isinstance(X, list):
+        # Ensure X is a list of strings or a Pandas Series
+        # print('type of X:', type(X))    
+        if isinstance(X, list) or isinstance(X, pd.Series):
             return [
                 ' '.join([self.lemmatizer.lemmatize(self.stemmer.stem(word)) 
                         for word in text.split()])
                 for text in X
             ]
         else:
-            raise ValueError("Input to LemmatizerStemmer.transform must be a list of strings")
+            raise ValueError("Input to LemmatizerStemmer.transform must be a list of strings or Pandas Series")
 
